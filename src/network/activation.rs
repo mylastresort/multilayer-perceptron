9f9453763d2033a773bb7f319435480995268fbe
mylastresort <samytamim0@gmail.com>
2026-03-24 18:37:34 +0100

    refactor(network): unify activation implementations

--- src/network/activation.rs
+++ src/network/activation.rs
@@ -8,11 +8,6 @@ pub enum ActivationFunction {
     Softmax,
 }
 
-pub struct Sigmoid;
-pub struct Tanh;
-pub struct ReLU;
-pub struct Softmax;
-
 // Defines the trait for activation functions, which includes methods for forward and backward passes.
 pub trait Activation {
     // Function to compute the activation function on the input data.
@@ -25,87 +20,39 @@ pub trait Activation {
     fn backward(&self, x: &Array2<f64>, grad: &Array2<f64>) -> Array2<f64>;
 }
 
-impl Activation for Sigmoid {
+impl Activation for ActivationFunction {
     #[inline]
     fn as_function(&self, x: &Array2<f64>) -> Array2<f64> {
-        1.0 / (1.0 + (-x).mapv(f64::exp))
+        match self {
+            ActivationFunction::Sigmoid => x.mapv(|v| 1.0 / (1.0 + (-v).exp())),
+            ActivationFunction::Tanh => x.mapv(f64::tanh),
+            ActivationFunction::ReLU => x.mapv(|v| v.max(0.0)),
+            ActivationFunction::Softmax => {
+                let max = x.map_axis(Axis(1), |row| row.fold(f64::NEG_INFINITY, |a, &b| a.max(b)));
+                let exp_x = (x - &max.insert_axis(Axis(1))).mapv(f64::exp);
+                let sum_exp_x = exp_x.sum_axis(Axis(1));
+                exp_x / &sum_exp_x.insert_axis(Axis(1))
+            }
+        }
     }
 
     #[inline]
     fn derivative(&self, x: &Array2<f64>) -> Array2<f64> {
-        let sigmoid_x = self.as_function(x);
-        &sigmoid_x * &(1.0 - &sigmoid_x)
-    }
-
-    #[inline]
-    fn forward(&self, x: &Array2<f64>) -> Array2<f64> {
-        self.as_function(x)
-    }
-
-    #[inline]
-    fn backward(&self, x: &Array2<f64>, grad: &Array2<f64>) -> Array2<f64> {
-        grad * self.derivative(x)
-    }
-}
-
-impl Activation for Tanh {
-    #[inline]
-    fn as_function(&self, x: &Array2<f64>) -> Array2<f64> {
-        x.mapv(f64::tanh)
-    }
-
-    #[inline]
-    fn derivative(&self, x: &Array2<f64>) -> Array2<f64> {
-        let tanh_x = self.as_function(x);
-        tanh_x.mapv(|v| 1.0 - v * v)
-    }
-
-    #[inline]
-    fn forward(&self, x: &Array2<f64>) -> Array2<f64> {
-        self.as_function(x)
-    }
-
-    #[inline]
-    fn backward(&self, x: &Array2<f64>, grad: &Array2<f64>) -> Array2<f64> {
-        grad * self.derivative(x)
-    }
-}
-
-impl Activation for ReLU {
-    #[inline]
-    fn as_function(&self, x: &Array2<f64>) -> Array2<f64> {
-        x.mapv(|v| v.max(0.0))
-    }
-
-    #[inline]
-    fn derivative(&self, x: &Array2<f64>) -> Array2<f64> {
-        x.mapv(|v| if v > 0.0 { 1.0 } else { 0.0 })
-    }
-
-    #[inline]
-    fn forward(&self, x: &Array2<f64>) -> Array2<f64> {
-        self.as_function(x)
-    }
-
-    #[inline]
-    fn backward(&self, x: &Array2<f64>, grad: &Array2<f64>) -> Array2<f64> {
-        grad * self.derivative(x)
-    }
-}
-
-impl Activation for Softmax {
-    #[inline]
-    fn as_function(&self, x: &Array2<f64>) -> Array2<f64> {
-        let max = x.map_axis(Axis(1), |row| row.fold(f64::NEG_INFINITY, |a, &b| a.max(b)));
-        let exp_x = (x - &max.insert_axis(Axis(1))).mapv(f64::exp);
-        let sum_exp_x = exp_x.sum_axis(Axis(1));
-        exp_x / &sum_exp_x.insert_axis(Axis(1))
-    }
-
-    #[inline]
-    fn derivative(&self, x: &Array2<f64>) -> Array2<f64> {
-        let softmax_x = self.as_function(x);
-        &softmax_x * &(1.0 - &softmax_x)
+        match self {
+            ActivationFunction::Sigmoid => {
+                let sigmoid_x = self.as_function(x);
+                sigmoid_x.mapv(|v| v * (1.0 - v))
+            }
+            ActivationFunction::Tanh => {
+                let tanh_x = self.as_function(x);
+                tanh_x.mapv(|v| 1.0 - v * v)
+            }
+            ActivationFunction::ReLU => x.mapv(|v| if v > 0.0 { 1.0 } else { 0.0 }),
+            ActivationFunction::Softmax => {
+                let softmax_x = self.as_function(x);
+                &softmax_x * &(1.0 - &softmax_x)
+            }
+        }
     }
 
     #[inline]
@@ -121,7 +68,7 @@ impl Activation for Softmax {
 
 #[cfg(test)]
 mod tests {
-    use super::{Activation, ReLU, Sigmoid, Softmax, Tanh};
+    use super::{Activation, ActivationFunction};
     use ndarray::{Array2, Axis, arr2};
 
     fn assert_matrix_close(actual: &Array2<f64>, expected: &Array2<f64>, tol: f64) {
@@ -142,7 +89,7 @@ mod tests {
 
     #[test]
     fn sigmoid_forward_and_derivative_are_correct() {
-        let activation = Sigmoid;
+        let activation = ActivationFunction::Sigmoid;
         let x = arr2(&[[0.0, 1.0], [-1.0, 2.0]]);
 
         let y = activation.forward(&x);
@@ -163,7 +110,7 @@ mod tests {
 
     #[test]
     fn tanh_forward_and_derivative_are_correct() {
-        let activation = Tanh;
+        let activation = ActivationFunction::Tanh;
         let x = arr2(&[[0.0, 1.0], [-1.0, 2.0]]);
 
         let y = activation.forward(&x);
@@ -184,7 +131,7 @@ mod tests {
 
     #[test]
     fn relu_forward_derivative_and_backward_are_correct() {
-        let activation = ReLU;
+        let activation = ActivationFunction::ReLU;
         let x = arr2(&[[-1.0, 0.0, 2.0]]);
         let grad = arr2(&[[2.0, 2.0, 2.0]]);
 
@@ -203,7 +150,7 @@ mod tests {
 
     #[test]
     fn softmax_rows_sum_to_one() {
-        let activation = Softmax;
+        let activation = ActivationFunction::Softmax;
         let x = arr2(&[[1.0, 2.0, 3.0], [1000.0, 1001.0, 1002.0]]);
 
         let y = activation.forward(&x);
@@ -216,7 +163,7 @@ mod tests {
 
     #[test]
     fn softmax_is_shift_invariant_per_row() {
-        let activation = Softmax;
+        let activation = ActivationFunction::Softmax;
         let x = arr2(&[[1.0, 2.0, 3.0]]);
         let shifted = arr2(&[[101.0, 102.0, 103.0]]);
 
